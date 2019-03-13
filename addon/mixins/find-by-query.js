cd61bdb36a20cdd453597f03bea3fd480f381866
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-03-13 19:17:48 +0100

    documented mixin

--- addon/mixins/find-by-query.js
+++ addon/mixins/find-by-query.js
@@ -1,18 +1,29 @@
 import { Promise } from 'rsvp';
 import Mixin from '@ember/object/mixin';
 
-// FindByQuery is a Route mixin that allows specifying a query when searching
-// for models.
+/**
+ * FindByQuery is a Route mixin that allows specifying a query when searching for models.
+ */
 export default Mixin.create({
+  /**
+   * Will find a single model using a query.
+   *
+   * @param model The name of the model.
+   * @param query The filter attributes.
+   * @returns {Promise}
+   */
   findByQuery(model, query) {
+    // compute query
     let newQuery = {};
     Object.keys(query).forEach(function(key) {
       newQuery['filter[' + key + ']'] = query[key];
     });
 
     return new Promise((resolve, reject) => {
+      // query endpoint
       this.store.query(model, newQuery).then(
         result => {
+          // return first object on success
           resolve(result.objectAt(0));
         },
         err => {
