e9bea31632184174d9c7f9e8f74cbe19e2de17a3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-03-14 15:22:40 +0100

    improved automatic rollback mixin

--- addon/mixins/automatic-rollback.js
+++ addon/mixins/automatic-rollback.js
@@ -1,25 +1,43 @@
 import Mixin from '@ember/object/mixin';
 
-// TODO: Expose abandon confirmation as a callback.
-
 /**
  * AutomaticRollback is a Route mixin that on every transition checks if the model has dirty attributes.
  * It will ask the users to abandon the changes and rollback the model.
  */
 export default Mixin.create({
+  /**
+   * The callback that is run to determine if a transition should be aborted.
+   *
+   * @returns boolean
+   */
+  abandonCallback() {
+    return confirm('Are you sure you want to abandon your changes?');
+  },
+
   actions: {
     willTransition(transition) {
-      if (
-        this.controller.get('model.hasDirtyAttributes') &&
-        !confirm('Are you sure you want to abandon your changes?')
-      ) {
+      // get model
+      let model = this.controller.get('model');
+
+      // abort transition if model has dirty attributes and changes should not be abandoned
+      if(model.get('hasDirtyAttributes') && !this.abandonCallback()) {
+        // abort transition
         transition.abort();
-      } else {
-        if (this.controller.get('model.isNew')) {
-          this.controller.get('model').unloadRecord();
-        } else {
-          this.controller.get('model').rollbackAttributes();
-        }
+
+        return;
+      }
+
+      // changes can be abandoned, if any
+
+      // unload model if new
+      if (model.get('isNew')) {
+        model.unloadRecord();
+        return;
+      }
+
+      // rollback dirty attributes if changed
+      if(model.get('hasDirtyAttributes')) {
+          model.rollbackAttributes();
       }
     }
   }
