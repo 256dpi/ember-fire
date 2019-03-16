d3feed3aafa8cf8951b00bc233a22732901f7bb6
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-03-16 18:19:31 +0100

    use https

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: 'http://0.0.0.0:8000',
+  host: 'https://0.0.0.0:8000',
   namespace: 'v1/api',
 
   authorize(xhr) {
--- tests/dummy/app/services/watch.js
+++ tests/dummy/app/services/watch.js
@@ -1,5 +1,5 @@
 import Watch from '@256dpi/ember-fire/services/watch';
 
 export default Watch.extend({
-  watchURL: 'ws://0.0.0.0:8000/v1/api/watch'
+  watchURL: 'wss://0.0.0.0:8000/v1/api/watch'
 });
