d4346c3d99308b9f4424f9ebb7d9dffaf0035a2f
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-03-16 20:53:21 +0100

    simplified subscritpion

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,14 +1,4 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Route.extend(ApplicationRouteMixin, {
-  activate() {
-    this.watch.subscribe('items', {
-      state: true,
-    });
-  },
-
-  deactivate() {
-    this.watch.unsubscribe('items');
-  }
-});
+export default Route.extend(ApplicationRouteMixin);
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -3,6 +3,10 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Route.extend(AuthenticatedRouteMixin, {
   model() {
+    // ensure subscription
+    this.watch.subscribe('items', { state: true });
+
+    // get all items
     return this.store.findAll('item');
   }
 });
