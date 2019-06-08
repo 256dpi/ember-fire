ef6286d107c3c7aede0584194feaa54c9dfd2901
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-06-08 17:08:15 +0300

    use advanced fire features

--- tests/dummy/app/models/item.js
+++ tests/dummy/app/models/item.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   state: DS.attr('boolean'),
-  count: DS.attr('number')
+  count: DS.attr('number'),
+  createToken: DS.attr('string'),
+  updateToken: DS.attr('string')
 });
--- tests/dummy/app/routes/new.js
+++ tests/dummy/app/routes/new.js
@@ -3,8 +3,14 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 import AutomaticRollback from '@256dpi/ember-fire/mixins/automatic-rollback';
 
+function randomString() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
 export default Route.extend(AuthenticatedRouteMixin, AutomaticRollback, {
   model() {
-    return this.store.createRecord('item');
+    return this.store.createRecord('item', {
+      createToken: randomString(),
+    });
   }
 });
