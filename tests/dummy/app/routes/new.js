0b183077c7255d552b8e225e47e95db2fa9d3448
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-07-28 15:05:54 +0300

    fmt

--- tests/dummy/app/routes/new.js
+++ tests/dummy/app/routes/new.js
@@ -4,13 +4,20 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import AutomaticRollback from '@256dpi/ember-fire/mixins/automatic-rollback';
 
 function randomString() {
-  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  return (
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    Math.random()
+      .toString(36)
+      .substring(2, 15)
+  );
 }
 
 export default Route.extend(AuthenticatedRouteMixin, AutomaticRollback, {
   model() {
     return this.store.createRecord('item', {
-      createToken: randomString(),
+      createToken: randomString()
     });
   }
 });
