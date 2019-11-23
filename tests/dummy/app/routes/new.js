cde666252aeabd9a9589fa9259c16af65e481d08
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-11-23 17:44:57 +0100

    extracted random util

--- tests/dummy/app/routes/new.js
+++ tests/dummy/app/routes/new.js
@@ -3,16 +3,7 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 import AutomaticRollback from '@256dpi/ember-fire/mixins/automatic-rollback';
 
-function randomString() {
-  return (
-    Math.random()
-      .toString(36)
-      .substring(2, 15) +
-    Math.random()
-      .toString(36)
-      .substring(2, 15)
-  );
-}
+import { randomString } from '../utils';
 
 export default Route.extend(AuthenticatedRouteMixin, AutomaticRollback, {
   model() {
