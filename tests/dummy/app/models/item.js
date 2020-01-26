2854d02893e1223e960189528f6aebc062a70276
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-01-26 23:34:13 +0200

    added blob url helper

--- tests/dummy/app/models/item.js
+++ tests/dummy/app/models/item.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import { stringAsBlobURL } from '@256dpi/ember-fire/blob';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
@@ -10,28 +11,15 @@ export default DS.Model.extend({
   createToken: DS.attr('string'),
   updateToken: DS.attr('string'),
 
-  blobURL: computed('blob.bytes', function() {
-    if (!this.get('blob.bytes')) {
-      return undefined;
+  blobURL: computed('blob.{bytes,type}', function() {
+    if (this.get('blob.bytes')) {
+      return stringAsBlobURL(this.get('blob.bytes'), this.get('blob.type'));
     }
-
-    let b = atob(this.get('blob.bytes'));
-    let ab = new ArrayBuffer(b.length);
-    let ia = new Uint8Array(ab);
-    for (let i = 0; i < b.length; i++) {
-      ia[i] = b.charCodeAt(i);
-    }
-
-    let blob = new Blob([ia.buffer], { type: this.get('blob.type') });
-
-    return URL.createObjectURL(blob);
   }),
 
   fileURL: computed('file.view-key', function() {
-    if (!this.get('file.view-key')) {
-      return undefined;
+    if (this.get('file.view-key')) {
+      return `http://0.0.0.0:8000/api/download?key=${this.get('file.view-key')}`;
     }
-
-    return `http://0.0.0.0:8000/api/download?key=${this.get('file.view-key')}`;
   })
 });
