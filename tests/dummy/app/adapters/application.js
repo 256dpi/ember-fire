d09c18924df22ed4600011bfee01d0f2726d1196
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-01-27 10:19:09 +0200

    use new headers computed property

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,12 +1,17 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: 'http://0.0.0.0:8000',
   namespace: 'api',
 
-  authorize(xhr) {
-    let { access_token } = this.get('session.data.authenticated');
-    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
-  }
+  headers: computed('session.data.authenticated.access_token', function() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+    }
+
+    return headers;
+  }),
 });
