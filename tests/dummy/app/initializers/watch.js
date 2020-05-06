f0a98edaf17a6c4f4db1543b9a69049a470106a1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-05-06 16:29:32 +0300

    simplify

--- tests/dummy/app/initializers/watch.js
+++ tests/dummy/app/initializers/watch.js
@@ -1,6 +1,6 @@
 export default {
   name: 'watch',
-  initialize: function(app) {
+  initialize(app) {
     app.inject('route', 'watch', 'service:watch');
     app.inject('controller', 'watch', 'service:watch');
     app.inject('component', 'watch', 'service:watch');
