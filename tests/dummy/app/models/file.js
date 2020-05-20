634b7b452704d92fabda23009f77aea119b26837
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-05-20 09:34:41 +0200

    reflect changes in fire

--- tests/dummy/app/models/file.js
+++ tests/dummy/app/models/file.js
@@ -4,7 +4,7 @@ export default class extends Model {
   @attr('string') state;
   @attr('date') updatedAt;
   @attr('string') type;
-  @attr('number') length;
+  @attr('number') size;
   @attr handle;
   @attr('string') binding;
   @attr('string') owner;
