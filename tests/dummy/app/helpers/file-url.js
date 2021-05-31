4023b848a606ba7bf511f6f52470198d7ac36d5c
Joël Gähwiler <joel.gaehwiler@gmail.com>
2021-05-31 19:34:00 +0200

    inlined helper

--- tests/dummy/app/helpers/file-url.js
+++ tests/dummy/app/helpers/file-url.js
@@ -1,3 +1,10 @@
-import FileURL from '@256dpi/ember-fire/helpers/file-url';
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
 
-export default FileURL;
+export default class FileURL extends Helper {
+  @service files;
+
+  compute([link]) {
+    return this.files.url(link);
+  }
+}
