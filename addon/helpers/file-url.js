4f5df8623fe357c35afdef841a5cea75edb1d598
Joël Gähwiler <joel.gaehwiler@gmail.com>
2021-05-31 15:00:02 +0200

    fix name

--- addon/helpers/file-url.js
+++ addon/helpers/file-url.js
@@ -1,7 +1,7 @@
 import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
 
-export default class Transition extends Helper {
+export default class FileURL extends Helper {
   @service files;
 
   compute([link]) {
