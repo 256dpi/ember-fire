1dba4fbdeaf4f7ad2b95511ca06e518b93a6d879
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-07-13 09:14:47 +0200

    removed route handling logic

--- tests/dummy/app/components/form.js
+++ tests/dummy/app/components/form.js
@@ -1,19 +1,7 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class extends Component {
-  @service router;
-
-  passThrough = false;
-
-  constructor() {
-    super(...arguments);
-
-    // add listener for route change
-    this.router.on('routeWillChange', this, 'routeWillChange');
-  }
-
   get dirty() {
     // determine if dirty
     if (this.args.changeset) {
@@ -43,92 +31,11 @@ export default class extends Component {
     // handle unsaved new models
     if (this.args.model.isNew) {
       this.args.model.unloadRecord();
-      this.passThrough = true;
     } else if (this.dirty) {
       this.reset();
     }
 
     // call callback
     this.args.onCancel();
-  }
-
-  routeWillChange(transition) {
-    // check pass through
-    if (this.passThrough) {
-      return;
-    }
-
-    // ignore aborted transitions
-    if (transition.isAborted) {
-      return;
-    }
-
-    // ignore deleted models
-    if (this.args.model.isDeleted) {
-      return;
-    }
-
-    // handle unsaved new models
-    if (this.args.model.isNew) {
-      // discard model immediately if true
-      if (this.args.onAbandon === true) {
-        this.args.model.unloadRecord();
-        return;
-      }
-
-      // otherwise ignore if not set
-      if (!this.args.onAbandon) {
-        return;
-      }
-
-      // otherwise abort transition
-      transition.abort();
-
-      // call abandon callback
-      this.args.onAbandon().then((ok) => {
-        if (ok) {
-          // unload model and retry transition with pass through
-          this.args.model.unloadRecord();
-          this.passThrough = true;
-          transition.retry();
-        }
-      });
-
-      return;
-    }
-
-    // handle unsaved changed models
-    if (this.dirty) {
-      // rollback ack model immediately if true
-      if (this.args.onAbandon === true) {
-        this.reset();
-        return;
-      }
-
-      // otherwise ignore if not set
-      if (!this.args.onAbandon) {
-        return;
-      }
-
-      // otherwise abort transition
-      transition.abort();
-
-      // call abandon callback
-      this.args.onAbandon().then((ok) => {
-        if (ok) {
-          // rollback model and retry transition with pass through
-          this.reset();
-          this.passThrough = true;
-          transition.retry();
-        }
-      });
-    }
-  }
-
-  willDestroy() {
-    super.willDestroy();
-
-    // remove listener for route change
-    this.router.off('routeWillChange', this, 'routeWillChange');
   }
 }
