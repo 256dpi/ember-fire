73da4728d37171ce7ec4129c62156e2e6ef88a65
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-07-25 12:11:17 +0200

    removed computed usage

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,6 +1,5 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object'; // eslint-disable-line
 
 export default class extends JSONAPIAdapter {
   @service session;
@@ -8,7 +7,6 @@ export default class extends JSONAPIAdapter {
   host = 'http://localhost:8000';
   namespace = 'api';
 
-  @computed('session.{isAuthenticated,data.authenticated.access_token}') // eslint-disable-line
   get headers() {
     let headers = {};
     if (this.session.isAuthenticated) {
